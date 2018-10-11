078f0e573d0d84d5a0a615dccc9d45b4c4634911
rainstorm <wangrenqing_1993@126.com>
2018-10-12 01:56:34 +0800

    First commit

---
+++ a.go
@@ -0,0 +1,18 @@
+package a
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+func Feed(args ...string) error {
+	if len(args) == 0 {
+		return errors.New("so hungry~")
+	}
+	for _, food := range args {
+		fmt.Println("eat ", food, " ...")
+	}
+	fmt.Println("Ge~~~")
+	return nil
+}
